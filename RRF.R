832572cb65101366b6809b098471e2790120549d
souvikbag <sbag036@outlook.com>
2021-11-09 18:39:17 +0530

    RRF

---
+++ RRF.R
@@ -0,0 +1,100 @@
+install.packages("Boruta")
+library(Boruta)
+library(randomForest)
+
+
+
+rrf_simulation <- function(n,m)
+  
+{ 
+
+  
+  start <- Sys.time()
+  
+  X = matrix(rnorm(n*m,0,1),nrow = n,ncol = m)
+  ####X should be n*(m+1) matrix
+  
+  #####there are 100 regressors, so generate beta values such that 
+  #first 10 beta valuesare high and last 90 beta values are very low
+  beta <- numeric(m)
+  beta[1:(m/10)] <- runif(m/10,5,5)
+  
+  
+  ####we get the value of log(p/(1-p)) and errors are taken normal
+  logitp = X%*%beta + rnorm(n)
+  ######by some manipulation we get the value of p(y=1)
+  p = exp(logitp)/(1 +exp(logitp) )
+  #Using this value of p, we get the binary response variable
+  Y <- rbinom(n,1,p)
+  
+  #####merge X and Y to create one dataframe
+  sim_data <- as.data.frame(cbind(Y,X))
+  #logit_model <- glm(Y~.,data=sim_data, family = binomial)
+  ###alorithm did not converge
+  names(sim_data)[-1] <- paste0('x', 1:(ncol(sim_data)-1))
+  ## Train Test split
+  training.sample <- sim_data$Y %>% createDataPartition(p = 0.8 , list = FALSE) 
+  
+  train.data <- sim_data[training.sample,]
+  test.data <- sim_data[-training.sample,]
+  
+  #######Now we fit lasso
+  Y.train <- train.data$Y
+  Y.test <- test.data$Y
+  Y.train <- as.factor(Y.train)
+  Y.test <- as.factor(Y.test)
+  #model matrix
+  # Intercept term must not be included
+  X.train <- (model.matrix(Y.train~.,data = train.data))[,-c(1,2)]
+  X.test <- model.matrix(Y.test~.,data = test.data)[,-c(1,2)]
+  X.train = data.matrix(X.train)
+  #ordinary random forest.
+  rf <- RRF(X.train,Y.train, flagReg = 0, importance = TRUE)
+  impRF <- rf$importance
+  impRF <- impRF[,"MeanDecreaseGini"]
+  #guided regularized random forest
+  imp <- impRF/(max(impRF))#normalize the importance score
+  gamma <- 0.5
+  coefReg <- (1-gamma)+gamma*imp #weighted average
+  grrf <- RRF(X.train, Y.train, coefReg=coefReg, flagReg=1, importance = TRUE)
+  
+  # Selected set of features used for checking prediction accuracy
+  a <- grrf$feaSet
+  selected <- length(a)
+  selected1 <- sum(ifelse(a <= m/10 , 1, 0))
+  # when Not regularized this may happen
+  
+  #rf2 <-RRF(X.train,Y.train, flagReg = 1, importance = TRUE)
+  # rf2$feaSet
+  
+  # prediction using selected features
+  varimp <- X.test[,a]
+  rfimp <- randomForest(varimp,Y.test)
+  
+  cm <- table(Predicted = rfimp$predicted, Actual = Y.test)
+  miscla <- 1 - (sum(diag(cm))/sum(cm))
+  precision <- cm[1,1]/(cm[1,1]+cm[1,2])
+  recall <- cm[1,1]/(cm[1,1]+cm[2,1])
+  ##error frame
+  
+  # Time taken
+  stop <- Sys.time()
+  time.taken <- round((stop-start),2)
+
+  
+  ##error frame
+  errors <- data.frame(matrix(ncol = 6, nrow = 1))
+  colnames(errors) <- c("selected","selected_Imp","Misclassification_Rate", "Precision", "Recall", "Time")
+  errors$selected <- selected
+  errors$selected_Imp <- selected1
+  #errors$MSE <- round(mse_beta,2)
+  #errors$MAE <- round(mae_beta,2)
+  errors$Misclassification_Rate <- miscla
+  errors$Precision <- round(precision,2)
+  errors$Recall <- round(recall,2)
+  errors$Time <- time.taken
+  
+  return(list(errors,a))
+} 
+
+rrf_simulation(20000,500)
