87a60bd8009f1d228e9dba432baf24ea1d365476
souvikbag <sbag036@outlook.com>
2021-11-09 17:11:56 +0530

    BORUTA

---
+++ Boruta.R
@@ -0,0 +1,81 @@
+install.packages("Boruta")
+library(Boruta)
+library(randomForest)
+
+
+
+boruta_simulation <- function(n,m)
+
+  { 
+   
+  start <- Sys.time()
+  
+  X = matrix(rnorm(n*m,0,1),nrow = n,ncol = m)
+  ####X should be n*(m+1) matrix
+  
+  #####there are 100 regressors, so generate beta values such that 
+  #first 10 beta valuesare high and last 90 beta values are very low
+  beta <- numeric(m)
+  beta[1:(m/10)] <- runif(m/10,5,5)
+  
+  
+  ####we get the value of log(p/(1-p)) and errors are taken normal
+  logitp = X%*%beta + rnorm(n)
+  ######by some manipulation we get the value of p(y=1)
+  p = exp(logitp)/(1 +exp(logitp) )
+  #Using this value of p, we get the binary response variable
+  Y <- rbinom(n,1,p)
+  
+  #####merge X and Y to create one dataframe
+  sim_data <- as.data.frame(cbind(Y,X))
+  #logit_model <- glm(Y~.,data=sim_data, family = binomial)
+  ###alorithm did not converge
+  names(sim_data)[-1] <- paste0('x', 1:(ncol(sim_data)-1))
+  ## Train Test split
+  training.sample <- sim_data$Y %>% createDataPartition(p = 0.8 , list = FALSE) 
+  
+  train.data <- sim_data[training.sample,]
+  test.data <- sim_data[-training.sample,]
+  
+  #######Now we fit lasso
+  Y.train <- train.data$Y
+  Y.test <- test.data$Y
+  Y.train <- as.factor(Y.train)
+  Y.test <- as.factor(Y.test)
+  #model matrix
+  # Intercept term must not be included
+  X.train <- (model.matrix(Y.train~.,data = train.data))[,-c(1,2)]
+  X.test <- model.matrix(Y.test~.,data = test.data)[,-c(1,2)]
+  # Fit Sure Variable selecting using random forest
+  X.train = data.matrix(X.train)
+  boruta <- Boruta(X.train,Y.train, maxRuns = 500)  
+  a <- as.numeric(substr(getSelectedAttributes(boruta),2,5))
+  selected <- length(getSelectedAttributes(boruta))
+  selected1 <- sum(ifelse(a <= m/10 , 1, 0))
+  
+  # prediction using selected features
+  varimp <- X.test[,a]
+  rfimp <- randomForest(varimp,Y.test)
+  
+  cm <- table(Predicted = rfimp$predicted, Actual = Y.test)
+  
+  # Time taken
+  stop <- Sys.time()
+  time.taken <- round((stop-start),2)
+  
+  ##error frame
+  errors <- data.frame(matrix(ncol = 6, nrow = 1))
+  colnames(errors) <- c("selected","selected_Imp","Misclassification_Rate", "Precision", "Recall", "Time")
+  errors$selected <- selected
+  errors$selected_Imp <- selected1
+  #errors$MSE <- round(mse_beta,2)
+  #errors$MAE <- round(mae_beta,2)
+  errors$Misclassification_Rate <- miscla
+  errors$Precision <- round(precision,2)
+  errors$Recall <- round(recall,2)
+  errors$Time <- time.taken
+  
+  return(list(errors,a))
+} 
+
+boruta_simulation(1000,100)
